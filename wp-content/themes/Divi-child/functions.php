8c4575505f2f54993e0536a78dda437ae7fa2cc6
Nikesh <nikesh@kazistudios.com>
2015-09-30 13:32:52 +0545

    Search template created.

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -95,3 +95,25 @@ function get_all_posts_name() {
   $posts_name = array('post','news-events', 'team');
   return $posts_name;
 }
+
+//search
+function search_excerpt_highlight() {
+ $excerpt = get_the_excerpt();
+ //for page that used divi layouts
+ if($excerpt == '') {
+  truncate_post( 80 );
+ }
+ $keys = implode('|', explode(' ', get_search_query()));
+ $excerpt = preg_replace('/(' . $keys .')/iu', '<strong class="search-highlight">\0</strong>', $excerpt);
+
+ echo '<p>' . $excerpt . '</p>';
+}
+
+
+function search_title_highlight() {
+ $title = get_the_title();
+ $keys = implode('|', explode(' ', get_search_query()));
+ $title = preg_replace('/(' . $keys .')/iu', '<strong class="search-highlight">\0</strong>', $title);
+
+ echo $title;
+}
