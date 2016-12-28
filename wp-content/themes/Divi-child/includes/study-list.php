1e5e4a601abf84336673ca3ab43252ec14d586f3
Nicholas Diesslin <ndiesslin@augeogreer.com>
2016-12-28 09:40:29 -0600

    Further improved study query. Added labels to search form.

--- wp-content/themes/Divi-child/includes/study-list.php
+++ wp-content/themes/Divi-child/includes/study-list.php
@@ -2,7 +2,8 @@
 // Default meta query options, we will be adding more items to this array for filtering
 $meta_query = array( 'relation' => 'AND' );
 
-echo get_query_var('paged');
+// Set page id so filterItems gets correct id to hide
+$pageId = get_the_ID();
 
 // Query parameters
 function queryParameters() {
@@ -38,33 +39,57 @@ function setQueryValue($key, $value) {
   );
 }
 
+// Search query to get search array
+function setSearchValue($query) {
+  // Set page id as global so filterItems gets correct id to hide
+  global $pageId;
+
+  $args = array(
+    // Exclude page listing posts
+    'post__not_in' => array($pageId),
+    'post_type' => 'our-studies',
+    's' => $query,
+  );
+
+  return $args;
+}
+
 // Filter items that may be set in parameters
 function filterItems() {
   // Set meta query as global so it can be manipulated
   global $meta_query;
 
+  // Set page id as global so filterItems gets correct id to hide
+  global $pageId;
+
   // Check for paged
-  $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+  //$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
 
   // Set up arguments for query
   $args = array(
+    // Exclude page listing posts
+    'post__not_in' => array($pageId),
     'post_type' => 'our-studies',
     'meta_query' => $meta_query,
-    'paged' => $paged,
+    //'paged' => $paged,
+    // TODO: figure out what to order things by
+    // 'orderby' => array(
+    //   'team-last-name' => 'ASC'
+    // ),
+    'order' => 'ASC',
+    //'posts_per_page' => 1,
   );
 
   return $args;
 }
 
 
-// Allow searching without terms https://www.relevanssi.com/knowledge-base/using-relevanssi-without-a-search-term/
+//Allow searching without terms https://www.relevanssi.com/knowledge-base/using-relevanssi-without-a-search-term/
 add_filter('relevanssi_hits_filter', 'rlv_hits_filter');
 function rlv_hits_filter($hits) {
 
   // Get meta fiters from filter items array
   $args = filterItems();
-
-  //print_r($args);
 
   if ($hits[0] == null) {
     // no search hits, so must create new
@@ -81,7 +106,10 @@ function rlv_hits_filter($hits) {
   return $hits;
 }
 
-// Use CS instead of s to set search
+// Initialize default query, Relevanssi builds off of this
+$query = new WP_Query();
+
+//Use CS instead of s to set search
 if (isset($_GET['cs']) && !empty($_GET['cs'])) {
   $query->query_vars['s'] = $_GET['cs'];
 }
@@ -92,11 +120,15 @@ $query->query_vars['post_type'] = 'our-studies';
 // Limit post count per page
 $query->query_vars['posts_per_page'] = 1;
 
+// Enable paged for relevanssi
+$paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+$query->query_vars['paged'] = $paged;
+
 // Build relevanssi query, may not be neccesary, but will use for not incase relevanssi has some handy search features
 relevanssi_do_query($query);
 
-// print out list of results
-// Build divs befor layout
+// Build list of results
+// Build divs before layout
 $post_list = '
 <div class="et_pb_section page et_pb_section_0 et_section_regular">
   <div class="et_pb_row et_pb_row_0">
@@ -129,7 +161,7 @@ foreach ($query->posts as $r_post) {
   $post_list .= '<p>'.$conditions.'</p>';
   // Post excerpt
   if (has_excerpt($r_post->ID)) {
-    $post_list .= '<p>'.$excerpt.'<br><a href='.$link.'>'.$snippetLink.'</p>';
+    $post_list .= '<p>'.$excerpt.'<br><a href='.$link.'>'.$snippetLink.'</a></p>';
   } else {
     // Get content and strip all shortcodes.
     $content = get_the_content(); // Get post content in $content
@@ -140,16 +172,116 @@ foreach ($query->posts as $r_post) {
     $post_list .= $content;
   }
   $post_list .= '</div>';
+  //$post_list .= wp_pagenavi(array( 'query' => $query ));
 }
 
+// Post count
+$post_list .= '<br><br><p>Found '.$query->found_posts.' hits.</p>';
+
 // Close wrapper around posts
-$post_list .= '</div>';
-$post_list .= wp_pagenavi();
-// TODO: figure out page navigation
-//printf(wp_pagenavi());
-//print_r(wp_pagenavi( array( 'query' => $query->posts ) ));
-//printf(wp_pagenavi( array( 'query' => $query ) ));
 $post_list .= '</div></div></div>';
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* WP query without plugin
+$meta_args = filterItems();
+
+
+// if (isset($_GET['cs']) && !empty($_GET['cs'])) {
+//   $search_args = setSearchValue($_GET['cs']);
+// } else {
+//   $search_args = '';
+// }
+
+//Merge arguments for meta query filter and search filter
+$merged_query_args = array_merge( $meta_args, $meta_args );
+
+// Create empty query and populate with both results
+$wp_query = new WP_Query($merged_query_args);
+if ( have_posts() ) :
+  ?>
+  <div class="et_pb_section page et_pb_section_0 et_section_regular">
+    <div class="et_pb_row et_pb_row_0">
+      <div class="et_pb_column et_pb_column_4_4  et_pb_column_0">
+        <div class="et_pb_posts et_pb_module et_pb_bg_layout_light et_pb_blog_0">
+          <?php
+            while ( have_posts() ) : the_post(); ?>
+              <article class="et_pb_post team type-team status-publish has-post-thumbnail hentry category-research-physicians category-team clearfix">
+                <a href="<?php the_permalink(); ?>" class="entry-featured-image-url">
+                  <?php the_post_thumbnail('team-thumbnail'); ?>
+                </a>
+                <h2 class="entry-title">
+                  <a href="<?php the_permalink(); ?>">
+                    <?php the_title(); ?>
+                  </a>
+                </h2>
+                <p>
+                  <?php
+                    // Check for excerpt first.
+                    if (has_excerpt()) {
+                      echo get_the_excerpt();
+                    }
+                    // If there is no excerpt, grab the content
+                    else {
+                      // Get content and strip all shortcodes.
+                      $content = get_the_content(); // Get post content in $content
+                      $content = preg_replace("/\[(.*?)\]/i", '', $content); // strip shortcodes.
+                      $content = str_replace("&nbsp;", '', $content); // Strip any non breaking spaces.
+                      $content = trim($content); // Remove whitespace in begining and end.
+                      $content = wp_trim_words($content, 27, '...'); // Trim content to 27 words and end with ...
+                      echo $content;
+                    }
+                  ?>
+                </p>
+                <a href="<?php the_permalink(); ?>" class="more-link">
+                  View Profile
+                </a>
+              </article>
+            <?php endwhile; ?>
+        </div>
+        <?php
+          //Add pagination.
+          echo wp_pagenavi();
+        ?>
+      </div>
+    </div>
+  </div>
+<?php endif;
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Echo all results once here
 echo $post_list;
+
+// Pagination section
+?>
+<div class="et_pb_section page et_pb_section_0 et_section_regular">
+  <div class="et_pb_row et_pb_row_0">
+    <div class="et_pb_column et_pb_column_4_4  et_pb_column_0">
+      <div class="et_pb_posts et_pb_module et_pb_bg_layout_light et_pb_blog_0">
+        <?php wp_pagenavi(array( 'query' => $query )); ?>
+      </div>
+    </div>
+  </div>
+</div>
--- wp-content/themes/Divi-child/includes/study/search-form.php
+++ wp-content/themes/Divi-child/includes/study/search-form.php
@@ -19,41 +19,41 @@
               <div class="et_pb_column et_pb_column_1_2  et_pb_column_1">
                 <div>
                   <h3 class="text--purple display-inline text-bold">I AM A PHYSICIAN:</h3><br>
-                  <input type="checkbox" name="wpcf-physician-check" value="Yes" <?php echo in_array_r('Yes', getParameters(), false, 'wpcf-physician-check') ? 'checked' : ''; ?>>Yes<br>
-                  <input type="checkbox" name="wpcf-physician-check" value="No" <?php echo in_array_r('No', getParameters(), false, 'wpcf-physician-check') ? 'checked' : ''; ?>>No
+                  <input type="checkbox" name="wpcf-physician-check" value="Yes" id="physician-yes" <?php echo in_array_r('Yes', getParameters(), false, 'wpcf-physician-check') ? 'checked' : ''; ?>><label for="physician-yes">Yes</label><br>
+                  <input type="checkbox" name="wpcf-physician-check" value="No" id="physician-no" <?php echo in_array_r('No', getParameters(), false, 'wpcf-physician-check') ? 'checked' : ''; ?>><label for="physician-no">No</label>
                 </div>
                 <br>
                 <div>
                   <h3 class="text--purple display-inline text-bold">CONDITIONS</h3><br>
                   <div class="et_pb_column et_pb_column_1_2  et_pb_column_1">
-                    <input type="checkbox" name="wpcf-condition" value="Arrhythmia"<?php echo in_array_r('Arrhythmia', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Arrhythmia<br>
-                    <input type="checkbox" name="wpcf-condition" value="Cardiomyopathy"<?php echo in_array_r('Cardiomyopathy', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Cardiomyopathy<br>
-                    <input type="checkbox" name="wpcf-condition" value="Heart Attack"<?php echo in_array_r('Heart Attack', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Heart Attack<br>
-                    <input type="checkbox" name="wpcf-condition" value="Heart Failure"<?php echo in_array_r('Heart Failure', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Heart Failure<br>
-                    <input type="checkbox" name="wpcf-condition" value="Heart Valve Disease"<?php echo in_array_r('Heart Valve Disease', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Heart Valve Disease
+                    <input type="checkbox" name="wpcf-condition" value="Arrhythmia" id="Arrhythmia" <?php echo in_array_r('Arrhythmia', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="Arrhythmia">Arrhythmia</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Cardiomyopathy" id="Cardiomyopathy" <?php echo in_array_r('Cardiomyopathy', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="Cardiomyopathy">Cardiomyopathy</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Heart Attack" id="HeartAttack" <?php echo in_array_r('Heart Attack', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="HeartAttack">Heart Attack</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Heart Failure" id="HeartFailure" <?php echo in_array_r('Heart Failure', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="HeartFailure">Heart Failure</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Heart Valve Disease" id="HeartValveDisease" <?php echo in_array_r('Heart Valve Disease', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="HeartValveDisease">Heart Valve Disease</label>
                   </div>
                   <div class="et_pb_column et_pb_column_1_2  et_pb_column_1">
-                    <input type="checkbox" name="wpcf-condition" value="High Blood Pressure"<?php echo in_array_r('High Blood Pressure', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>High Blood Pressure<br>
-                    <input type="checkbox" name="wpcf-condition" value="High Cholesterol"<?php echo in_array_r('High Cholesterol', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>High Cholesterol<br>
-                    <input type="checkbox" name="wpcf-condition" value="Peripheral Arterial Disease"<?php echo in_array_r('Peripheral Arterial Disease', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Peripheral Arterial Disease<br>
-                    <input type="checkbox" name="wpcf-condition" value="Sudden Cardiac Arrest"<?php echo in_array_r('Sudden Cardiac Arrest', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Sudden Cardiac Arrest<br>
-                    <input type="checkbox" name="wpcf-condition" value="Stroke"<?php echo in_array_r('Stroke', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>>Stroke<br>
+                    <input type="checkbox" name="wpcf-condition" value="High Blood Pressure" id="HighBloodPressure" <?php echo in_array_r('High Blood Pressure', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="HighBloodPressure">High Blood Pressure</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="High Cholesterol" id="HighCholesterol" <?php echo in_array_r('High Cholesterol', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="HighCholesterol">High Cholesterol</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Peripheral Arterial Disease" id="PeripheralArterialDisease" <?php echo in_array_r('Peripheral Arterial Disease', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="PeripheralArterialDisease">Peripheral Arterial Disease</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Sudden Cardiac Arrest" id="SuddenCardiacArrest" <?php echo in_array_r('Sudden Cardiac Arrest', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="SuddenCardiacArrest">Sudden Cardiac Arrest</label><br>
+                    <input type="checkbox" name="wpcf-condition" value="Stroke" id="Stroke" <?php echo in_array_r('Stroke', getParameters(), false, 'wpcf-condition') ? 'checked' : ''; ?>><label for="Stroke">Stroke</label><br>
                   </div>
                 </div>
               </div>
               <div class="et_pb_column et_pb_column_1_2  et_pb_column_1">
                 <div>
                   <h3 class="text--purple display-inline text-bold">GENDER</h3><br>
-                  <input type="checkbox" name="wpcf-gender" value="Male" <?php echo in_array_r('Male', getParameters(), false, 'wpcf-gender') ? 'checked' : ''; ?>>Male
-                  <input type="checkbox" name="wpcf-gender" value="Female" <?php echo in_array_r('Female', getParameters(), false, 'wpcf-gender') ? 'checked' : ''; ?>>Female
+                  <input type="checkbox" name="wpcf-gender" value="Male" id="Male" <?php echo in_array_r('Male', getParameters(), false, 'wpcf-gender') ? 'checked' : ''; ?>><label for="Male">Male</label>
+                  <input type="checkbox" name="wpcf-gender" value="Female" id="Female" <?php echo in_array_r('Female', getParameters(), false, 'wpcf-gender') ? 'checked' : ''; ?>><label for="Female">Female</label>
                 </div>
                 <br>
                 <div>
                   <h3 class="text--purple display-inline text-bold">AGE</h3><br>
-                  <input type="checkbox" name="wpcf-age-range" value="18-20" <?php echo in_array_r('18-20', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>>18-20<br>
-                  <input type="checkbox" name="wpcf-age-range" value="21-74"<?php echo in_array_r('21-74', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>>21-74<br>
-                  <input type="checkbox" name="wpcf-age-range" value="75-79"<?php echo in_array_r('75-79', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>>75-79<br>
-                  <input type="checkbox" name="wpcf-age-range" value="80+"<?php echo in_array_r('80+', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>>80+
+                  <input type="checkbox" name="wpcf-age-range" value="18-20" id="18-20" <?php echo in_array_r('18-20', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>><label for="18-20">18-20</label><br>
+                  <input type="checkbox" name="wpcf-age-range" value="21-74" id="21-74" <?php echo in_array_r('21-74', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>><label for="21-74">21-74</label><br>
+                  <input type="checkbox" name="wpcf-age-range" value="75-79" id="75-79" <?php echo in_array_r('75-79', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>><label for="75-79">75-79</label><br>
+                  <input type="checkbox" name="wpcf-age-range" value="80" id="80" <?php echo in_array_r('80+', getParameters(), false, 'wpcf-age-range') ? 'checked' : ''; ?>><label for="80">80+</label>
                 </div>
               </div>
               <!-- <div class="et_pb_column et_pb_column_1_2  et_pb_column_1">
