3b0c88df5f32a13465fecfe7bcf9ec52911ab446
Nicholas Diesslin <ndiesslin@augeogreer.com>
2017-01-05 16:43:50 -0600

    Adjusted toggle.

--- wp-content/themes/Divi-child/js/custom.js
+++ wp-content/themes/Divi-child/js/custom.js
@@ -238,8 +238,8 @@ function loadToggled() {
     if (localStorage.getItem('study-form-toggle') == null) {
       $('.group-toggle__tab').each( function() {
         var targetId = $(this).data('toggle-id');
-        $('#'+targetId).addClass('group-toggle-target--toggled');
-        $(this).parent('.group-toggle').addClass('group-toggle--toggled');
+        $('#'+targetId).removeClass('group-toggle-target--toggled');
+        $(this).parent('.group-toggle').removeClass('group-toggle--toggled');
       });
     }
     // If toggle is set to open add class
