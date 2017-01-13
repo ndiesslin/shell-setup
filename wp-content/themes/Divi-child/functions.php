cb455d69aaa4a0247565c8ca6e70669067b1c8b1
Nicholas Diesslin <ndiesslin@augeogreer.com>
2017-01-13 16:26:32 -0600

    removed filter and added remove filter to strip paragraph tags.

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -572,21 +572,4 @@ function app_icons() {
 }
 
 // Strip all empty p tags
-add_filter( 'the_content', 'remove_empty_p', 20, 1 );
-function remove_empty_p( $content ){
-  // clean up p tags around block elements
-  $content = preg_replace( array(
-    '#<p>\s*<(div|aside|section|article|header|footer)#',
-    '#</(div|aside|section|article|header|footer)>\s*</p>#',
-    '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
-    '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
-    '#<p>\s*</(div|aside|section|article|header|footer)#',
-  ), array(
-    '<$1',
-    '</$1>',
-    '</$1>',
-    '<$1$2>',
-    '</$1',
-  ), $content );
-  return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', $content);
-}
+remove_filter('the_content', 'wpautop');
