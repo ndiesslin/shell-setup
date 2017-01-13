c57e78b1aaa938a0837748170d7c467f5634de04
Nicholas Diesslin <ndiesslin@augeogreer.com>
2017-01-13 16:22:14 -0600

    testing a different way to strip empty content.

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -572,8 +572,21 @@ function app_icons() {
 }
 
 // Strip all empty p tags
-add_filter('the_content', 'remove_empty_p', 20, 1);
-function remove_empty_p($content){
-  $content = force_balance_tags($content);
-  return preg_replace('#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content);
+add_filter( 'the_content', 'remove_empty_p', 20, 1 );
+function remove_empty_p( $content ){
+  // clean up p tags around block elements
+  $content = preg_replace( array(
+    '#<p>\s*<(div|aside|section|article|header|footer)#',
+    '#</(div|aside|section|article|header|footer)>\s*</p>#',
+    '#</(div|aside|section|article|header|footer)>\s*<br ?/?>#',
+    '#<(div|aside|section|article|header|footer)(.*?)>\s*</p>#',
+    '#<p>\s*</(div|aside|section|article|header|footer)#',
+  ), array(
+    '<$1',
+    '</$1>',
+    '</$1>',
+    '<$1$2>',
+    '</$1',
+  ), $content );
+  return preg_replace('#<p>(\s|&nbsp;)*+(<br\s*/*>)*(\s|&nbsp;)*</p>#i', '', $content);
 }
