cf9a350a1a551cd5eb5c7ac559d4e014581892b7
Nicholas Diesslin <ndiesslin@augeomarketing.com>
2018-03-26 14:23:23 -0500

    AD-381 created first finalized iteration of search section.

--- wp-content/themes/Divi-child/includes/shortcodes/search-input-template.php
+++ wp-content/themes/Divi-child/includes/shortcodes/search-input-template.php
@@ -1,18 +1,31 @@
-<form role="search" method="get" action="/" class="wpcf7-form">
-  <input type="search" placeholder="Enter a Keyword, topic, name, etc..." value="" name="s" title="Search for:" class="wpcf7-form-control form-input wpcf7-text">
-  <select name="post-type">
-    <option>Search All</option>
-    <?php
-      $args = array(
-        'public'              => true,
-        'exclude_from_search' => false
-      );
-      $output = 'objects';
-      $post_types = get_post_types( $args, $output );
-      foreach ( $post_types  as $post_type ) {
-        echo '<option value="' . $post_type->name . '" name="post-type">' . $post_type->label . '</option>';
-      }
-    ?>
-  </select>
-  <input type="submit" value="Search" class="wpcf7-form-control wpcf7-submit">
+<form role="search" method="get" action="/" class="display-flex flex-wrap-wrap">
+  <div class="search-module display-flex flex-wrap-wrap">
+    <input type="search" placeholder="Enter a Keyword, topic, name, etc..." value="" name="s" title="Search for:" class="search-module__input">
+    <label class="search-module__select-label margin-bottom--20">
+      <select name="post-type" class="search-module__select">
+        <option>Search All</option>
+        <?php
+          $args = array(
+            'public'              => true,
+            'exclude_from_search' => false
+          );
+          $output = 'objects';
+          $post_types = get_post_types( $args, $output );
+          foreach ( $post_types as $post_type ) {
+            echo $post_type->name;
+            $exclude = array( 'project', 'homepage-slider', 'attachment' ); // Excludes projects, homepage sliders, and attachments
+
+            if( TRUE === in_array( $post_type->name, $exclude ) )
+              continue;
+
+            echo '<option value="' . $post_type->name . '" name="post-type">' . $post_type->label . '</option>';
+          }
+        ?>
+      </select>
+      <i class="fa fa-chevron-down search-module__select-arrow"></i>
+    </label>
+  </div>
+  <button type="submit" class="search-button">
+    <i class="fa fa-search"></i> Search
+  </button>
 </form>
