791fd7ed2f6c88746ef71917a65fb7dbffd3d20d
nicholas diesslin <nicholasdiesslin@gmail.com>
2018-11-02 16:36:40 +0000

    Removed wordfence, it's not compatible with pantheon.

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"GeX`6l,gl( Ef)%SB/f)&=+*8$yF0Bp`?[dj0#SgFu7Sa,Glend$X>9[^<mgXlH]";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3643;}
